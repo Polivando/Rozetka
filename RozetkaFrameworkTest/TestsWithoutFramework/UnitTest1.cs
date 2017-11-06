aa45c9223c8edfb01b0d1fabf3378cc6e827c57a
Polivando <Polivando@POLIVANDOLAPTOP>
2017-11-06 10:41:49 +0200

    deleted unused code

--- RozetkaFrameworkTest/TestsWithoutFramework/UnitTest1.cs
+++ RozetkaFrameworkTest/TestsWithoutFramework/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿//using NUnit.Framework;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
